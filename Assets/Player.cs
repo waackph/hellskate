c595c1504d60b4a218c9f5cd324b613d018f2cf7
Philipp Waack <waackphilipp@gmail.com>
2022-03-05 11:26:21 +0100

    add camera and player movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,20 +5,94 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    [SerializeField] Vector2 jumpVelocity;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float moveImpact;
+    [SerializeField] float laneYPosition;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float jumpGravityUp;
+    [SerializeField] float jumpGravityDown;
+    [SerializeField] float buttonTime;
+    Rigidbody2D rigidBody;
+    bool isJumping = false;
+    float jumpTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            GetComponent<Rigidbody2D>().velocity = jumpVelocity;
+        jumpWithVelocity();
+    }
 
+    void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("YOU DIED!");
+    }
+
+    void movePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(movementSpeed + moveX * moveImpact, rigidBody.velocity.y);
+    }
+
+    void jumpWithVelocity()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Transform>().position.y == laneYPosition) 
+        {
+            isJumping = true;
+            jumpTime = 0;
+        }
+        if(isJumping)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpHeight);
+            jumpTime += Time.deltaTime;
+        }
+        if((Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime) && GetComponent<Transform>().position.y != laneYPosition)
+        {
+            isJumping = false;
+            rigidBody.gravityScale = jumpGravityUp;
+        }
+        if(GetComponent<Transform>().position.y < laneYPosition)
+        {
+            rigidBody.gravityScale = 0;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, laneYPosition, 0);
+        }
+    }
+
+    void jumpWithForce()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            rigidBody.gravityScale = jumpGravityUp;
+            float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rigidBody.gravityScale));
+            // rigidBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        if(rigidBody.velocity.y <= 0 && isJumping)
+        {
+            if(GetComponent<Transform>().position.y <= laneYPosition)
+            {
+                rigidBody.gravityScale = 0;
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, laneYPosition, 0);
+                isJumping = false;
+            }
+            else
+            {
+                rigidBody.gravityScale = jumpGravityDown;
+            }
         }
     }
 }
--- Documentation/unity_notes.md
+++ Documentation/unity_notes.md
@@ -7,5 +7,22 @@
 
 ## Update vs. FixedUpdate
 - Update differs between devices, because different devices can play different amount of frames per second
-- FixedUpdate has always the same amount of calls in a time unit. e.g. for movement depending on update should be manageed in FixedUpdate.
+- FixedUpdate has always the same amount of calls in a time unit. e.g. for movement depending on update should be managed in FixedUpdate.
 
+## Move Player
+- If for some reason we do not want a RigidBody2D attached (which is recommended when making use of collision detection and movement), the transform can be used to move the player `transform.position + moveSpeed * Time.deltaTime`.
+- [x] Using a RigidBody2D we need to use its velocity instead of manipulating the transform directly (this would cause unpredictable behavior). `rigidBody2D.velocity = new Vector2(moveX, moveY) * movementSpeed;`. For this to work in a 2D game with top down (no ground collision object), we will need to set the gravity to 0 and set a constraint to rotation.
+
+## Moving the Camera
+- To move the camera we could manipulate the transforms position itself (like for a player without RigidBody), using direct Matrix Operations we may better use `transform.Translate(new Vector3(cameraSpeed, 0, 0) * Time.deltaTime)`
+
+## Input Movement
+- To get direct movement input independet of input device use: `Input.GetAxis("Horizontal")` and `Input.GetAxis("Vertical")` 
+
+## Player Jump
+- A jump with a rigidBody should be implemented using the AddForce Function `rigidBody.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse)`. Because the gravity is set to 0, we need to set it higher so the force (adding velocity) is reduced by gravity again. We need to detect when the jump is finished (the ground is hit (a specific y-position)) and need to set the y-Position to the ground (because checking just the y-position and turning off gravity will likely not lead to a precise positioning but can be below or above the aimed y-Position). <br>
+This is a workaround - not sure if it will lead to problems..
+- [x] We could also use the `rigidBody.velocity.y` directly to make a jump
+- Furthermore we can use the transform.position.y to make a jump, but with a rigidBody attached this may lead to issues. In this case we would have to calculate our own gravity and so on.
+
+For further information on implementing the different ways in unity, see: https://gamedevbeginner.com/how-to-jump-in-unity-with-or-without-physics/
