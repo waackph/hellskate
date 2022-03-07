9f8b6059361d002362f42f31b69ce49f21b3ad04
Philipp Waack <waackphilipp@gmail.com>
2022-03-07 23:17:52 +0100

    add lane system

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,35 +5,59 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] GameObject laneSystem;
     [SerializeField] float movementSpeed;
     [SerializeField] float moveImpact;
-    [SerializeField] float laneYPosition;
     [SerializeField] float jumpHeight;
     [SerializeField] float jumpGravityUp;
     [SerializeField] float jumpGravityDown;
     [SerializeField] float buttonTime;
     Rigidbody2D rigidBody;
-    bool isJumping = false;
+
+    Lane currentLane;
+
+    bool isUpper = false;
     float jumpTime;
+
+    // State (switch lane, jump) variables
+    bool isInSwitchMode = false;
+    bool isJumpingUp = false;
+    bool isInJumpMode = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        currentLane = laneSystem.GetComponent<LaneController>().CurrentLane;
+        HandleLaneCollision();
     }
 
     // Update is called once per frame
     void Update()
     {
-        jumpWithVelocity();
+        if(!isInSwitchMode && !isInJumpMode)
+        {
+            checkJumpWithVelocity();
+        }
+        if(!isInJumpMode && !isInSwitchMode)
+        {
+            checkSwitchLanes();
+        }
     }
 
     void FixedUpdate()
     {
         movePlayer();
+        if(isInSwitchMode)
+        {
+            doMoveToLane();
+        }
+        if(isInJumpMode)
+        {
+            doJump();
+        }
     }
 
-    //Upon collision with another GameObject, this GameObject will reverse direction
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("YOU DIED!");
@@ -45,49 +69,116 @@ public class Player : MonoBehaviour
         rigidBody.velocity = new Vector2(movementSpeed + moveX * moveImpact, rigidBody.velocity.y);
     }
 
-    void jumpWithVelocity()
+    void checkSwitchLanes()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Transform>().position.y == laneYPosition) 
+        float moveY = Input.GetAxis("Vertical");
+        if(moveY > 0)
         {
-            isJumping = true;
+            isUpper = true;
+            switchLane(laneUp:isUpper);
+        }
+        else if(moveY < 0)
+        {
+            isUpper = false;
+            switchLane(laneUp:isUpper);
+        }
+    }
+
+    void switchLane(bool laneUp)
+    {
+        isInSwitchMode = true;
+        currentLane = laneSystem.GetComponent<LaneController>().SwitchLane(laneUp);
+        HandleLaneCollision();
+    }
+
+    void HandleLaneCollision()
+    {
+        if(currentLane.Name == "Lane1")
+        {
+            Physics2D.IgnoreLayerCollision(0, 6, false);
+            Physics2D.IgnoreLayerCollision(0, 7, true);
+            Physics2D.IgnoreLayerCollision(0, 8, true);
+        }
+        else if(currentLane.Name == "Lane2")
+        {
+            Physics2D.IgnoreLayerCollision(0, 6, true);
+            Physics2D.IgnoreLayerCollision(0, 7, false);
+            Physics2D.IgnoreLayerCollision(0, 8, true);
+        }
+        else if(currentLane.Name == "Lane3")
+        {
+            Physics2D.IgnoreLayerCollision(0, 6, true);
+            Physics2D.IgnoreLayerCollision(0, 7, true);
+            Physics2D.IgnoreLayerCollision(0, 8, false);
+        }
+    }
+
+    void doMoveToLane()
+    {
+        if((currentLane.LaneYPosition <= GetComponent<Transform>().position.y && isUpper) || (currentLane.LaneYPosition >= GetComponent<Transform>().position.y && !isUpper))
+        {
+            isInSwitchMode = false;
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, currentLane.LaneYPosition, 0);
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+        }
+        else
+        {
+            if(isUpper)
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, movementSpeed);
+            else
+                rigidBody.velocity = new Vector2(rigidBody.velocity.x, -movementSpeed);
+        }
+    }
+
+    void checkJumpWithVelocity()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && GetComponent<Transform>().position.y == currentLane.LaneYPosition) 
+        {
+            isJumpingUp = true;
+            isInJumpMode = true;
             jumpTime = 0;
         }
-        if(isJumping)
+    }
+
+    void doJump()
+    {
+        if((Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime) && GetComponent<Transform>().position.y != currentLane.LaneYPosition)
+        {
+            isJumpingUp = false;
+            rigidBody.gravityScale = jumpGravityUp;
+        }
+        if(isJumpingUp)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpHeight);
             jumpTime += Time.deltaTime;
         }
-        if((Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime) && GetComponent<Transform>().position.y != laneYPosition)
-        {
-            isJumping = false;
-            rigidBody.gravityScale = jumpGravityUp;
-        }
-        if(GetComponent<Transform>().position.y < laneYPosition)
+        if(GetComponent<Transform>().position.y < currentLane.LaneYPosition && isInJumpMode)
         {
             rigidBody.gravityScale = 0;
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
-            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, laneYPosition, 0);
+            GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, currentLane.LaneYPosition, 0);
+            isInJumpMode = false;
         }
     }
 
     void jumpWithForce()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumpingUp)
         {
-            isJumping = true;
+            isJumpingUp = true;
             rigidBody.gravityScale = jumpGravityUp;
             float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * rigidBody.gravityScale));
             // rigidBody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
             rigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
-        if(rigidBody.velocity.y <= 0 && isJumping)
+        if(rigidBody.velocity.y <= 0 && isJumpingUp)
         {
-            if(GetComponent<Transform>().position.y <= laneYPosition)
+            if(GetComponent<Transform>().position.y <= currentLane.LaneYPosition)
             {
                 rigidBody.gravityScale = 0;
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
-                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, laneYPosition, 0);
-                isJumping = false;
+                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, currentLane.LaneYPosition, 0);
+                isJumpingUp = false;
             }
             else
             {
