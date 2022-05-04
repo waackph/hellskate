f0bd620c539fae7e21fb7bf75a4c1d13a0b4e03c
Philipp Waack <waackphilipp@gmail.com>
2022-05-04 19:30:21 +0200

    adjust lanes to have a reasonable height, correct position and make lane heights dependent to each other

--- Assets/LaneController.cs
+++ Assets/LaneController.cs
@@ -7,18 +7,21 @@ public class LaneController : MonoBehaviour
     Lane UpperLane;
     Lane MiddleLane;
     Lane LowerLane;
+    [SerializeField] float lowerLanePosition; // -4.2
+    [SerializeField] float laneHeight; // 1.26
+
     public Lane CurrentLane;
 
     void Awake()
     {
-        UpperLane = new Lane("Lane1", -0.5f);
-        MiddleLane = new Lane("Lane2", -1.5f);
-        LowerLane = new Lane("Lane3", -2.5f);
+        UpperLane = new Lane("Lane1", lowerLanePosition+laneHeight*2);
+        MiddleLane = new Lane("Lane2", lowerLanePosition+laneHeight);
+        LowerLane = new Lane("Lane3", lowerLanePosition);
         UpperLane.LaneBelow = MiddleLane;
         MiddleLane.LaneAbove = UpperLane;
         MiddleLane.LaneBelow = LowerLane;
         LowerLane.LaneAbove = MiddleLane;
-        CurrentLane = MiddleLane;
+        CurrentLane = LowerLane;
     }
 
     // Start is called before the first frame update
